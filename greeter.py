2e4a1f83b96b99753071cff248d1faa44136a705
FernandezTur <turcarjfer@gmail.com>
2021-09-14 01:10:00 -0400

    same

--- greeter.py
+++ greeter.py
@@ -1,4 +1,4 @@
-# Writing clear prompts
+## Writing clear prompts
 name = input("Please enter your name: ")
 print("Hello, " + name + "!")
 
@@ -7,3 +7,17 @@ prompt += "\nWhat is your first name? "
 
 name = input(prompt)
 print("\nHello, " + name + "!")
+
+## Defining a function
+def greet_user():
+	"""Display a simple greeting."""
+	print("Hello!")
+
+greet_user()
+
+## Passing information to a function
+def greet_user(username):
+	"""Display a simple greeting."""
+	print("Hello, " + username.title() + "!")
+
+greet_user('jesse')
